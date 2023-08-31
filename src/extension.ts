aca0c0b96b0e80b87a3c2b5ac7056ded9a932b53
luhaoyu <haoyu3@163.com>
2023-08-31 22:07:09 +0800

    Add Judge Json Code

--- src/extension.ts
+++ src/extension.ts
@@ -13,14 +13,78 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('rustjson.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('rustjson.Rust2Json', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from RustJson!');
+		// vscode.window.showInformationMessage('Hello World from RustJson!');
+
+
+		const editor = vscode.window.activeTextEditor;
+
+		if (editor) {
+			const selection = editor.selection;
+			const selectedCode = editor.document.getText(selection);
+
+			// Process selected code
+			if (!isJsonString(selectedCode)) {
+				return;
+
+			}
+
+			// console.log(selectedCode);
+			vscode.window.showInformationMessage(selectedCode);
+		}
+
+		const quickPick = vscode.window.createQuickPick();
+		quickPick.items = [
+			{ label: '1', description: '1' },
+			{ label: '2', description: '2' },
+			{ label: '3', description: '3' }
+		]
+		quickPick.onDidChangeSelection((selection) => {
+			vscode.window.showInformationMessage(selection[0].description ?? "");
+		});
+		quickPick.show();
+
+
+		// 输入
+		// const input = vscode.window.createInputBox();
+		// input.title = "RustJson";
+		// input.prompt = "Please input your Json";
+		// input.show();
+
+		// if (editor) {
+		// 	const line = 0; // 设置要选中的代码的起始行号
+		// 	const startCharacter = 0; // 设置要选中的代码的起始字符位置
+		// 	const endCharacter = 10; // 设置要选中的代码的结束字符位置
+
+		// 	const startPosition = new vscode.Position(line, startCharacter);
+		// 	const endPosition = new vscode.Position(line, endCharacter);
+		// 	const selectionRange = new vscode.Range(startPosition, endPosition);
+
+		// 	editor.selection = new vscode.Selection(selectionRange.start, selectionRange.end);
+		// }
+
+		/**
+		 * Judge the code you select is legal json
+		 * 
+		 * @param code the code you select
+		 * @returns is legal json string
+		 */
+		function isJsonString(code: string): boolean {
+			try {
+				JSON.parse(code);
+			} catch (error) {
+				vscode.window.showInformationMessage("Please select json");
+				return false;
+			}
+			return true;
+		}
 	});
+
 
 	context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
